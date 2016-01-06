d056e81307737c6bed3e1ee0cf9967d185c21cbe
J. von Grumbkow <jgrumbkow@gmail.com>
2016-01-06 10:55:08 +0100

    Init
    Initial release
    
    - adds notification setting to add uploads as attachments
    - adds fileuplodas as attachments when above setting is enabled

---
+++ gforms-uploads-as-attachments/gforms-uploads-as-attachments.php
@@ -0,0 +1,107 @@
+<?php
+/*
+Plugin Name: Gravity Forms - Uploads as Attachments
+Plugin URI: https://github.com/Jebble/Gravity-Forms-Uploads-as-Attachments
+Description: Adds an option to send files from the fileupload field(s) as attachment with notifications
+Version: 0.1.0
+Author: Jebble
+Author URI: http://jebble.nl/
+Text Domain: jbl_gfuaa
+*/
+
+add_action( 'admin_init', 'jbl_check_gforms' );
+function jbl_check_gforms() {
+    if ( is_admin() && !is_plugin_active( 'gravityforms/gravityforms.php' ) ) {
+        add_action( 'admin_notices', function() {
+        	echo '<div class="error"><p><strong>Gravity Forms - Uploads as Attachments</strong> requires <a href="http://www.gravityforms.com/" target="_blank"><em>Gravity Forms by Rocketgenius, Inc.</em></a> to work.</p></div>';
+        } );
+    }
+}
+
+
+/**
+ * Gravity Forms Notification UI Settings
+ *
+ * Add custom settings to the notification page in Gravity Forms
+ */
+add_filter( 'gform_notification_ui_settings', 'jbl_add_gform_notification_settings', 10, 3 );
+function jbl_add_gform_notification_settings( $ui_settings, $confirmation, $form ) {
+
+	/** Add fileuploads as attachments checkbox */
+	if( isset( $confirmation['jbl_gfuaa_enable'] ) && $confirmation['jbl_gfuaa_enable'] == 1 ) {
+		$checked = ' checked="checked"';
+	}
+	else {
+		$checked = '';
+	}
+
+	$ui_settings['jbl_uaa_settings'] = '
+		<tr valign="top">
+			<th scope="row">
+				<label for="jbl_gfuaa_enable">' . __( 'Uploads as Attachments', 'jbl_gfuaa' ) . '</label>
+			</th>
+			<td>
+				<input type="checkbox" id="jbl_gfuaa_enable" name="jbl_gfuaa_enable" value="1"' . $checked . '>
+				<label for="jbl_gfuaa_enable" class="inline">' . __( 'Add fileupload fields from this form as attachments to this notification', 'jbl_gfuaa' ) . '</label>
+				<br>
+			</td>
+		</tr>
+	';
+
+    return $ui_settings;
+}
+
+
+/**
+ * Save custom settings to the notification
+ */
+add_filter( 'gform_pre_notification_save', 'jbl_save_gform_notification_settings', 10, 2 );
+function jbl_save_gform_notification_settings( $notification, $form ) {
+
+	if( isset( $_POST['jbl_gfuaa_enable'] ) ) {
+    	$notification['jbl_gfuaa_enable'] = rgpost( 'jbl_gfuaa_enable' );
+	}
+
+    return $notification;
+}
+
+
+/**
+ * Add the fileupload files as attachments if the option is enabled for this notification
+ */
+add_filter( 'gform_notification', 'jbl_gfuaa_maybe_add_attachments', 10, 3 );
+function jbl_gfuaa_maybe_add_attachments( $notification, $form, $entry ) {
+
+	if( isset( $notification['jbl_gfuaa_enable'] ) && $notification['jbl_gfuaa_enable'] == 1 ) {
+
+		$fileuploadfields = GFCommon::get_fields_by_type( $form, array( 'fileupload' ) );
+
+		if( is_array( $fileuploadfields ) && ! empty( $fileuploadfields ) ) {
+
+			$attachments = array();
+			$upload_root = RGFormsModeL::get_upload_root();
+
+			foreach ( $fileuploadfields as $field ) {
+				$url = $entry[ $field['id'] ];
+			}
+
+			if ( empty( $url ) ) {
+                continue;
+            }
+            elseif ( $field['multipleFiles'] ) {
+                $uploaded_files = json_decode( stripslashes( $url ), true );
+                foreach ( $uploaded_files as $uploaded_file ) {
+                    $attachment = preg_replace( '|^(.*?)/gravity_forms/|', $upload_root, $uploaded_file );
+                    $attachments[] = $attachment;
+                }
+            } else {
+                $attachment = preg_replace( '|^(.*?)/gravity_forms/|', $upload_root, $url );
+                $attachments[] = $attachment;
+            }
+
+            $notification['attachments'] = $attachments;
+		}
+	}
+
+	return $notification;
+}
---
+++ gforms-uploads-as-attachments/readme.txt
@@ -0,0 +1,23 @@
+=== Gravity Forms - Uploads as Attachments ===
+Contributors: jebble
+Tags: gravity, forms, gforms, file, upload, uploads, attachment, attachments, notification, notifications
+Requires at least: 0.1.0
+Tested up to: 4.4
+Stable tag: trunk
+License: GNU GPL
+License URI: http://www.gnu.org/licenses/gpl-3.0.en.html
+
+Adds the option to add all fileupload fields no notifications as attachments
+
+== Description ==
+! Requires Gravity Forms by Rocketgenius, Inc.
+
+This plugin adds an option to notifications to add files uploaded to fileupload fields as attachment to that specific notification.
+
+== Installation ==
+Installation instructions for WordPress plugins can be found in the codex.
+https://codex.wordpress.org/Managing_Plugins#Installing_Plugins
+
+== Changelog ==
+0.1.0
+- Initial release
